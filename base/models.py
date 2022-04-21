a48d5269fb183e45012c0528033ae09a6a427954
Lemmy Mwaura <lemminton@gmail.com>
2022-04-21 13:29:14 +0300

    added Profile model and profile creation signal

--- base/models.py
+++ base/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+class Profile(models.Model):
+    user_profile = models.OneToOneField(User, related_name='currentuser', on_delete=models.CASCADE)
+    bio = models.TextField()
+    updated = models.DateTimeField(auto_now=True)
+    created = models.DateTimeField(auto_now_add=True)
+    
+    class Meta:
+        ordering = ['-updated', '-created']
+
+    def __str__(self):
+        return self.user_profile.username
